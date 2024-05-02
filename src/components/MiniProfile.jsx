5e7fd9008b14513205d52c570c88a1d0c5b3bf18
Valerii <valerabulon@gmail.com>
2024-05-02 20:31:35 +0300

    complete the mini profile component

--- src/components/MiniProfile.jsx
+++ src/components/MiniProfile.jsx
@@ -1,3 +1,29 @@
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function MiniProfile() {
-  return <div>MiniProfile</div>;
+  const { data: session } = useSession();
+  console.log(session);
+  return (
+    <div className="flex items-center justify-between mt-14 ml-10">
+      <img
+        src={session?.user?.image || "/instagram-logo.webp"}
+        alt="user-image"
+        className="w-16 h-16 p-[2px] rounded-full border"
+      />
+      <div className="flex-1 ml-4">
+        <h2 className="font-bold">{session?.user?.username}</h2>
+        <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
+      </div>
+      {session ? (
+        <button className="text-blue-500 font-semibold" onClick={signOut}>
+          Sign Out
+        </button>
+      ) : (
+        <button className="text-blue-500 font-semibold" onClick={signIn}>
+          Sign In
+        </button>
+      )}
+    </div>
+  );
 }
